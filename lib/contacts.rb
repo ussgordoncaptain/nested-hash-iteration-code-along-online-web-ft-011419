2b7593ca79651ad37c03dc3258782217e981a3a0
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-20 08:16:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,9 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-  flavors=contacts["Freddy Mercury"][:favorite_ice_cream_flavors]
-  final = []
-  flavors.each do |x|
-    delete_if
+   contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
   end
 end
